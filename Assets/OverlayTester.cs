cb052c6e95bbbda7fd76a17a81443a70da049ed9
Nicholas Ewalt <Hotrian@Gmail.com>
2016-06-11 02:32:03 -0700

    OpenVR Overlays for All!
    Init! A basic demo scene with two overlays! Check out OverlayTester.cs
    which shows how you can easily change overlay textures!

---
+++ Assets/OverlayTester.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlayTester : MonoBehaviour
+{
+    public HeadlessVROverlay Overlay;
+    public Texture2D TestTexture;
+	void Start ()
+    {
+        if (Overlay != null && TestTexture != null)
+        {
+            Overlay.SetTexture(TestTexture);
+        }
+	}
+}
